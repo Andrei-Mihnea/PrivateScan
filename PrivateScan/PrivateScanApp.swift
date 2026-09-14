a36a80660fc915d92aebf705238b0b60bafbdd03
Andrei-Mihnea <andrei.mihnea@student.tuiasi.ro>
2026-09-14 16:09:36 +0300

    Initial Commit

---
+++ PrivateScan/PrivateScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrivateScanApp.swift
+//  PrivateScan
+//
+//  Created by Mihnea Andrei on 14/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct PrivateScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
